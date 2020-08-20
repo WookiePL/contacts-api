eab0ef7a43fe529405181ca6c1427e8b461cbab1
Łukasz Janas <wookiepl@outlook.com>
2020-08-20 11:01:38 +0200

    Added H2 database + exemplary data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+--INSERT INTO CONTACTS VALUES(1, 'address', 'email', 'firstname', 'lastname', '123123123');
+INSERT INTO CONTACTS VALUES(1, 'Rue de la Mèbre 9, 1020 Renens', 'lukasz@janas.com', 'Lukasz', 'Janas', '0585736750');
+INSERT INTO CONTACTS VALUES(2, 'Route de la Maladière 6, 1022 Chavannes-près-Renens', 'marc@johnson.ch', 'Marc', 'Johnson', '0585735240');
+
+
