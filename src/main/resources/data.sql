e2f00abc692733bdb3f5310d6aec65d709a23c2a
Łukasz Janas <wookiepl@outlook.com>
2020-08-25 04:16:10 +0200

    Exemplary data.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,16 @@
 INSERT INTO CONTACTS VALUES(1, 'Rue de la Mèbre 9, 1020 Renens', 'lukasz@janas.com', 'Lukasz', 'Janas', '0585736750');
 INSERT INTO CONTACTS VALUES(2, 'Route de la Maladière 6, 1022 Chavannes-près-Renens', 'marc@johnson.ch', 'Marc', 'Johnson', '0585735240');
 
+INSERT INTO SKILLS VALUES(1, 'Advanced', 'Java');
+INSERT INTO SKILLS VALUES(2, 'Basic', 'Java');
+INSERT INTO SKILLS VALUES(3, 'Advanced', 'TypeScript');
+INSERT INTO SKILLS VALUES(4, 'Basic', 'TypeScript');
+INSERT INTO SKILLS VALUES(5, 'Expert', 'Java');
+
+INSERT INTO CONTACTS_SKILLS VALUES(1, 1);
+INSERT INTO CONTACTS_SKILLS VALUES(1, 4);
+INSERT INTO CONTACTS_SKILLS VALUES(2, 2);
+INSERT INTO CONTACTS_SKILLS VALUES(2, 3);
+
+
 
